9bc6b6f3e365de572a7e5e03534576981ff20a93
sameed02 <sameedahmad3@gmail.com>
2024-01-07 02:26:55 +0530

    implemented router using react router v6.4+

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-function App() {
-  return <div>Hello Vite</div>;
-}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
 export default App;
