446e8446e497c2556774aba52ee7ee1eb2d2de4a
sameed02 <sameedahmad3@gmail.com>
2024-01-07 17:45:13 +0530

    fetching order using fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
-
+import Error from "../src/ui/Error.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -26,6 +28,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
       {
         path: "/order/new",
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
   return (
     <header>
-      <Link to="/">Fast React Pizza</Link>
-      <p>Sameed</p>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
     </header>
   );
 }
