8f309354e2edb051447893f67abf9443288ae5a1
sameed02 <sameedahmad3@gmail.com>
2024-01-07 18:31:31 +0530

    creating order, POST req with React Router Actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as CreateOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "../src/ui/Error.jsx";
 const router = createBrowserRouter([
@@ -34,6 +36,7 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: CreateOrderAction,
       },
     ],
   },
